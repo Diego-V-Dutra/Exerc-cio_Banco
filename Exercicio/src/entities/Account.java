59c58b6111a1d3b80d5300c79460ba53a90b54b4
Diego Vinicius <103463251+Diego-V-Dutra@users.noreply.github.com>
2024-08-08 10:40:44 -0400

    Update Account.java

--- Exercicio/src/entities/Account.java
+++ Exercicio/src/entities/Account.java
@@ -5,8 +5,6 @@ public class Account {
     private String holder;
     private double balance;
     
-    
-    
     public Account(int number, String holder){
         this.number = number;
         this.holder = holder;
